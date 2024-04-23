dd1100beea0d37538a8195461650ad5b089fc0c3
usmanghani6080 <usman.hypersoft@gmail.com>
2024-04-23 01:02:56 -0700

    main updated with required changes.

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
-from diffusers import DiffusionPipeline
+
 import torch
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from diffusers import DiffusionPipeline, LCMScheduler
+from fastapi.responses import FileResponse
 
-# load both base & refiner
-base = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-)
-base.to("cuda")
-refiner = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
-    text_encoder_2=base.text_encoder_2,
-    vae=base.vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
+
+app = FastAPI()
+
+pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
     variant="fp16",
-)
-refiner.to("cuda")
+    torch_dtype=torch.float16
+).to("cuda")
 
-# Define how many steps and what % of steps to be run on each experts (80/20) here
-n_steps = 40
-high_noise_frac = 0.8
+# set scheduler
+pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
 
-prompt = "A majestic lion jumping from a big stone at night"
+# load LCM-LoRA
+pipe.load_lora_weights("latent-consistency/lcm-lora-sdxl")
 
-# run both experts
-image = base(
-    prompt=prompt,
-    num_inference_steps=n_steps,
-    denoising_end=high_noise_frac,
-    output_type="latent",
-).images
-image = refiner(
-    prompt=prompt,
-    num_inference_steps=n_steps,
-    denoising_start=high_noise_frac,
-    image=image,
-).images[0]
+@app.post("/generate_image/")
+async def generate_image(prompt: str):
+    try:
+        generator = torch.manual_seed(42)
+        image = pipe(
+            prompt=prompt, num_inference_steps=4, generator=generator, guidance_scale=1.0
+        ).images[0]
+
+        # Save the image to a file
+        output_file = "output_image.jpeg"
+
+        image.save(output_file, format="JPEG")
+
+        return FileResponse(output_file, media_type="image/jpeg")
+
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
